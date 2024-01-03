be66ba0ba0d02b4aec20e4bf1e9f4ce866cea762
Yumi Yukimura <me.cafebabe@gmail.com>
2024-01-04 02:11:22 +0800

    virt: common: Setup DHCP on Ethernet interface
    * ADB over network as well while we're at it

--- common/common.mk
+++ common/common.mk
@@ -3,3 +3,14 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
+# Network tools
+PRODUCT_PACKAGES += \
+    virt_dhcpclient
+
+# Properties
+PRODUCT_PRODUCT_PROPERTIES += \
+    service.adb.tcp.port=5555
+
+PRODUCT_VENDOR_PROPERTIES += \
+    vendor.recovery.dhcpclient.iface?=eth0
